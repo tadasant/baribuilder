468f88fb26fe6c554f421ca4d6905cc468b1008a
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-14 07:44:14 -0500

    global.d.ts file

--- baribuilder-ui/src/global.d.ts
+++ baribuilder-ui/src/global.d.ts
@@ -1,6 +1,8 @@
 declare module '*.svg'
 declare module '*.png'
 declare module '*.jpg'
+
+// Missing typings
 declare module 'react-facebook-pixel' {
   const ReactPixel: any;
   export default ReactPixel;
