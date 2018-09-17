463b648d75e118e40e40d6196723a3b67e00506f
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-17 09:41:01 -0500

    clarified some naming

--- baribuilder-ui/src/components/builder/building/ProductSelection.tsx
+++ baribuilder-ui/src/components/builder/building/ProductSelection.tsx
@@ -1,30 +1,13 @@
-/**
- * Explanation of the props typings... (9/14/18)
- *
- * The `graphql` function creates a HOC.
- *
- * That HOC has DataProps<GetProducts> as its props. The HOC requires that child components have ChildDataProps<{}, GetProducts> as their props.
- *
- * The displayLoadingState `branch` HOC takes in and requires the same props: ChildDataProps<{}, GetProducts> (so it fits with the graphql HOC)
- *
- * The pure component continues the trend, requiring ChildDataProps<{}, GetProducts>. That'll come from the compose chain.
- *
- * The pure component happens to not require any other props from the caller in the application code, so IProps is (implicitly) {}.
- */
-
-
 import gql from "graphql-tag";
 import * as React from 'react';
 import {SFC} from 'react';
-import {ChildDataProps, DataProps, graphql} from 'react-apollo';
+import {ChildDataProps, graphql} from 'react-apollo';
 import {branch, compose, pure, renderComponent} from 'recompose';
 import {GetProducts} from '../../../gql-typings/GetProducts';
 import Product from './Product';
 
-// GraphQL HOC props (input)
-type TOuterProps = DataProps<GetProducts>;
 // GraphQL HOC props (output)
-type ChildProps = ChildDataProps<{}, GetProducts>;
+type DataOutputProps = ChildDataProps<{}, GetProducts>;
 
 const data = graphql<{}, GetProducts>(gql`
     query GetProducts {
@@ -34,19 +17,19 @@ const data = graphql<{}, GetProducts>(gql`
     }
 `);
 
-const displayLoadingState = branch<ChildProps>(
+const displayLoadingState = branch<DataOutputProps>(
   (props) => props.data.loading,
   renderComponent(() => <div>Loading</div>),
 );
 
-const enhance = compose<TOuterProps, {}>(
+const enhance = compose<DataOutputProps, {}>(
   data,
   displayLoadingState,
   pure,
 );
 
 // Pure
-const ProductSelectionPure: SFC<ChildProps> = ({data: {allProducts}}) => {
+const ProductSelectionPure: SFC<DataOutputProps> = ({data: {allProducts}}) => {
   if (allProducts !== undefined) {
     return <div>{allProducts.map(o => <Product key={o.id} id={o.id}/>)}</div>;
   }
