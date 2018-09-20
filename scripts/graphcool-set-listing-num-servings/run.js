000d371d5c4208eff7ab93f40325a6caa6873088
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-20 16:16:46 -0500

    done with script that updates listing

---
+++ scripts/graphcool-set-listing-num-servings/run.js
@@ -0,0 +1,75 @@
+import { GraphQLClient } from 'graphql-request'
+
+const client = new GraphQLClient('https://api.graph.cool/simple/v1/cjlzqvawt1ib00107g3nfr04i', {
+  headers: {
+    Authorization: 'Bearer YOUR_AUTH_TOKEN',
+  },
+});
+
+
+function setItem() {
+  return client.request(`
+    {
+      allProducts {
+        listings {
+          id
+        }
+        nutritionFacts {
+          serving {
+            count
+          }
+        }
+      }
+    }
+
+    mutation {
+      updateListing(
+        id: "abc"
+        numServings: 100
+      ) {
+        id
+      }
+    }
+  `)
+}
+
+const getProducts = () => {
+  return client.request(`
+    {
+      allProducts {
+        listings {
+          id
+        }
+        nutritionFacts {
+          serving {
+            count
+          }
+        }
+      }
+    }
+  `);
+};
+
+const setListing = (id, numServings) => {
+  return client.request(`
+    mutation {
+      updateListing(
+        id: "${id}"
+        numServings: ${numServings}
+      ) {
+        id
+      }
+    }
+  `);
+};
+
+// getProducts()
+//   .then(data => {
+//     const products = data.allProducts;
+//     products.forEach(product => {
+//       const {id} = product.listings[0];
+//       const {count} = product.nutritionFacts.serving;
+//       setListing(id, count).then(data => console.log(data));
+//     });
+//   })
+//   .catch(error => console.log(error));
