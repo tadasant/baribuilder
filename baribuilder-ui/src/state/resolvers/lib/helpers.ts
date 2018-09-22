bd26c8768bf1f73a608bbd8c314611328f75044f
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-22 10:41:53 -0500

    adding regimens

--- baribuilder-ui/src/state/resolvers/lib/helpers.ts
+++ baribuilder-ui/src/state/resolvers/lib/helpers.ts
@@ -1,51 +1,12 @@
 import {cloneDeep, keyBy} from 'lodash';
 import {GetAllProductsIngredients_allProducts} from '../../../typings/gql/GetAllProductsIngredients';
-import {FREQUENCY, INGREDIENT_QUANTITY_UNITS} from '../../../typings/gql/globalTypes';
+import {FREQUENCY} from '../../../typings/gql/globalTypes';
 import {ICost, IIngredient, IIngredientRange, IRegimenIngredient, IRegimenProduct} from '../../client-schema-types';
 import {IProductForProjectedRegimenCost} from './product_projectedRegimenCost';
 
-
-interface IRegimenIngredientsByName {
-  [key: string]: IRegimenIngredient;
-}
-
-const calculateRegimenIngredients = (
-  regimenProducts: IRegimenProduct[],
-  products: GetAllProductsIngredients_allProducts[]
-): IRegimenIngredientsByName => {
-  const productsById = keyBy(products, product => product.id);
-
-  const result: IRegimenIngredientsByName = {};
-  regimenProducts.forEach(product => {
-    const ingredients = productsById[product.id].nutritionFacts.ingredients;
-    if (ingredients !== null) {
-      ingredients.forEach(ingredient => {
-        const regimenIngredient: IRegimenIngredient = {
-          ...cloneDeep(ingredient),
-          ingredientQuantity: {
-            ...cloneDeep(ingredient.ingredientQuantity),
-            amount: ingredient.ingredientQuantity.amount * product.quantity.number,
-          },
-          frequency: product.quantity.frequency,
-        };
-
-        if (!result.hasOwnProperty(ingredient.ingredientType.name)) {
-          result[ingredient.ingredientType.name] = regimenIngredient;
-        } else {
-          result[ingredient.ingredientType.name] = addRegimenIngredients(result[ingredient.ingredientType.name], regimenIngredient);
-        }
-      })
-    } else {
-      console.warn(`Ingredients shouldn\'t be null. Error code 589238. Product ID: ${product.id}`);
-    }
-  });
-
-  return result;
-};
-
 /**
  * Returns non-range ingredients with the minimum desired as a reference point. If the existing exceeds the
- * minimum, will omit from the results.
+ * minimum, will omit from the results. Assumes minimum of 0 if not set.
  */
 export const subtractRegimenIngredientsFromDesiredIngredientRanges = (
   regimenProducts: IRegimenProduct[],
@@ -53,16 +14,16 @@ export const subtractRegimenIngredientsFromDesiredIngredientRanges = (
   products: GetAllProductsIngredients_allProducts[],
 ): IRegimenIngredient[] => {
   const regimenIngredientsByName = calculateRegimenIngredients(regimenProducts, products);
-  const result: IRegimenIngredient[] = [];
+  const results: IRegimenIngredient[] = [];
 
   desiredIngredientRanges.forEach(range => {
-    const regimenIngredient = subtractRegimenIngredientFromMinimumIngredient(range, regimenIngredientsByName[range.ingredientType.name]);
-    if (regimenIngredient !== null) {
-      result.push(regimenIngredient)
+    const result = subtractRegimenIngredientFromMinimumIngredient(range, regimenIngredientsByName[range.ingredientType.name]);
+    if (result !== null && result.ingredientQuantity.amount > 0) {
+      results.push(result)
     }
   });
 
-  return result;
+  return results;
 };
 
 export const subtractProductFromRegimenIngredients = (
@@ -101,21 +62,76 @@ export const addCosts = (...costs: ICost[]): ICost => {
   };
 };
 
-const addRegimenIngredients = (...ingredients: IRegimenIngredient[]): IRegimenIngredient => {
-  // TODO
-  return {
-    __typename: 'RegimenIngredient',
-    frequency: FREQUENCY.DAILY,
-    ingredientQuantity: {
-      __typename: 'IngredientQuantity',
-      amount: 0.0,
-      units: INGREDIENT_QUANTITY_UNITS.G
-    },
-    ingredientType: {
-      __typename: 'IngredientType',
-      name: 'Vitamin A',
-    },
+/**
+ * Below are local helpers. In the weeds.
+ */
+
+interface IRegimenIngredientsByName {
+  [key: string]: IRegimenIngredient;
+}
+
+const calculateRegimenIngredients = (
+  regimenProducts: IRegimenProduct[],
+  products: GetAllProductsIngredients_allProducts[]
+): IRegimenIngredientsByName => {
+  const productsById = keyBy(products, product => product.id);
+
+  const result: IRegimenIngredientsByName = {};
+  regimenProducts.forEach(product => {
+    const ingredients = productsById[product.id].nutritionFacts.ingredients;
+    if (ingredients !== null) {
+      ingredients.forEach(ingredient => {
+        const regimenIngredient: IRegimenIngredient = {
+          ...cloneDeep(ingredient),
+          ingredientQuantity: {
+            ...cloneDeep(ingredient.ingredientQuantity),
+            amount: ingredient.ingredientQuantity.amount * product.quantity.number,
+          },
+          frequency: product.quantity.frequency,
+        };
+
+        if (!result.hasOwnProperty(ingredient.ingredientType.name)) {
+          result[ingredient.ingredientType.name] = regimenIngredient;
+        } else {
+          const totalRegimenIngredients = addRegimenIngredients(result[ingredient.ingredientType.name], regimenIngredient);
+          if (totalRegimenIngredients !== null) {
+            result[ingredient.ingredientType.name] = totalRegimenIngredients;
+          }
+        }
+      })
+    } else {
+      console.warn(`Ingredients shouldn\'t be null. Error code 589238. Product ID: ${product.id}`);
+    }
+  });
+
+  return result;
+};
+
+const addRegimenIngredients = (...ingredients: IRegimenIngredient[]): IRegimenIngredient | null => {
+  if (ingredients.length == 1) {
+    return ingredients[0];
+  } else if (ingredients.length == 2) {
+    if (ingredients[0].ingredientType.name !== ingredients[1].ingredientType.name) {
+      console.error(`${ingredients[0].ingredientType.name} !== ${ingredients[1].ingredientType.name}. This shouldn't happen. Error code 434829.`);
+      return null;
+    }
+    if (ingredients[0].ingredientQuantity.units !== ingredients[1].ingredientQuantity.units) {
+      console.warn('Unit conversions unsupported. Error code 434829');
+      return null;
+    }
+
+    return {
+      ...cloneDeep(ingredients[0]),
+      ingredientQuantity: {
+        ...cloneDeep(ingredients[0].ingredientQuantity),
+        amount: ingredients[0].ingredientQuantity.amount + ingredients[1].ingredientQuantity.amount,
+      }
+    }
   }
+
+  // TODO support recursive
+  console.warn('Recursion unsupported. Error code 434829');
+  return null;
 };
 
 const subtractRegimenIngredientFromMinimumIngredient = (
@@ -123,16 +139,29 @@ const subtractRegimenIngredientFromMinimumIngredient = (
   regimenIngredient: IRegimenIngredient
 ): IRegimenIngredient | null => {
   const {minimumIngredientQuantity} = range;
+  // Assume 0 minimum if not set
+  const minimumIngredientQuantityAmount = minimumIngredientQuantity === null ? 0 : minimumIngredientQuantity.amount;
 
+  if (range.ingredientType.name !== regimenIngredient.ingredientType.name) {
+    console.error(`${range.ingredientType.name} !== ${regimenIngredient.ingredientType.name}. This shouldn't happen. Error code 489293.`);
+    return null;
+  }
   if (range.frequency !== regimenIngredient.frequency) {
     console.warn('Frequency conversions unsupported. Error code 489293.');
     return null;
   }
-  if (minimumIngredientQuantity == null) {
-    console.warn('minimumIngredientQuantity was null. Error code 489293.');
-    return null;
+  if (minimumIngredientQuantity !== null) {
+    if (minimumIngredientQuantity.units !== regimenIngredient.ingredientQuantity.units) {
+      console.warn('Ingredient conversions unsupported. Error code 489293.');
+      return null;
+    }
   }
-  // if (minimumIngredientQuantity.units !==)
-  //
-  // return minimumIngredientQuantity.number;
+
+  return {
+    ...cloneDeep(regimenIngredient),
+    ingredientQuantity: {
+      ...cloneDeep(regimenIngredient.ingredientQuantity),
+      amount: minimumIngredientQuantityAmount - regimenIngredient.ingredientQuantity.amount,
+    },
+  }
 };
