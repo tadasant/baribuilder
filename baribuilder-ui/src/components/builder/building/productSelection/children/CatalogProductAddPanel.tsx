fe45d2bbf7caef278032ed7bb4bbe22624459c8c
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-24 11:14:50 -0500

    add button placeD

--- baribuilder-ui/src/components/builder/building/productSelection/children/CatalogProductAddPanel.tsx
+++ baribuilder-ui/src/components/builder/building/productSelection/children/CatalogProductAddPanel.tsx
@@ -1,13 +1,15 @@
-import {Grid} from '@material-ui/core';
+import {Button, Grid} from '@material-ui/core';
 import gql from 'graphql-tag';
 import * as React from 'react';
-import {Fragment, SFC} from 'react';
+import {SFC} from 'react';
 import {ChildDataProps, graphql} from 'react-apollo';
 import {compose, pure} from 'recompose';
+import styled from 'styled-components';
 import {
   GetCatalogProductQuantities,
   GetCatalogProductQuantitiesVariables
 } from '../../../../../typings/gql/GetCatalogProductQuantities';
+import {EmptyRow} from '../../../../style/Layout';
 
 const GET_CATALOG_PRODUCT_QUANTITIES_QUERY = gql`
     query GetCatalogProductQuantities($id: ID) {
@@ -39,17 +41,27 @@ const enhance = compose<IProps & DataOutputProps, IProps>(
   pure,
 );
 
+const OuterGrid = styled(Grid)`
+  height: 100%;
+`;
+
 // Pure
 const CatalogProductAddPanelPure: SFC<IProps & DataOutputProps> = ({data: {CatalogProduct}}) => {
   if (!CatalogProduct) {
     return null;
   }
   return (
-    <Fragment>
-      <Grid container alignItems='center'>
-        {CatalogProduct.quantity.amount}
-      </Grid>
-    </Fragment>
+    <OuterGrid item container direction='row' justify='space-evenly'>
+        <Grid item lg={12}>
+          {CatalogProduct.quantity.amount}
+        </Grid>
+        <EmptyRow mobile='0px'/>
+        <Grid item lg={4} />
+        <Grid item lg={4}>
+          <Button variant='raised' color='default' fullWidth>Add</Button>
+        </Grid>
+        <Grid item lg={4} />
+    </OuterGrid>
   );
 };
 
