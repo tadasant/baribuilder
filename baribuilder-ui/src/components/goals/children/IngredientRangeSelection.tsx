a3ea4d9a3516f22acd08657eeeaf6354a870ba81
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-09-26 15:54:55 -0500

    first select box visuals

--- baribuilder-ui/src/components/goals/children/IngredientRangeSelection.tsx
+++ baribuilder-ui/src/components/goals/children/IngredientRangeSelection.tsx
@@ -1,8 +1,14 @@
 import {Grid} from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import gql from 'graphql-tag';
 import * as React from 'react';
 import {SFC} from 'react';
+import {ChildDataProps, graphql} from 'react-apollo';
 import styled from 'styled-components';
+import Sketch from '../../../app/style/SketchVariables';
 import {IIngredientRange} from '../../../state/client-schema-types';
+import {GetIngredientTypes} from '../../../typings/gql/GetIngredientTypes';
 import {Body} from '../../style/Typography';
 import {HandleChangeGoalFunc} from '../GoalsScreen';
 
@@ -11,18 +17,52 @@ interface IProps {
   onChange: HandleChangeGoalFunc,
 }
 
+const GET_INGREDIENT_TYPES = gql`
+    query GetIngredientTypes {
+        allIngredientTypes {
+            name
+            defaultUnits
+            synonyms
+        }
+    }
+`;
+
+type ReferenceDataOutputProps = ChildDataProps<IProps, GetIngredientTypes>;
+
+const withReferenceData = graphql<IProps, GetIngredientTypes>(GET_INGREDIENT_TYPES);
+
 const CenteredBody = styled(Body)`
   text-align: center;
 `;
 
-const IngredientRangeSelection: SFC<IProps> = ({ingredientRange}) => {
+const ShadowedSelect = styled(Select)`
+  && {
+    box-shadow: 0px 1px 2px 0px ${Sketch.color.accent.grey};
+    font-weight: 800;
+    font-size: 1.5em;
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+const IngredientRangeSelection: SFC<ReferenceDataOutputProps & IProps> = ({ingredientRange, onChange, data: {allIngredientTypes}}) => {
+  const handleChangeIngredientTypeName: (event: React.ChangeEvent<HTMLSelectElement>) => void = (event) => {
+    onChange(ingredientRange.ingredientTypeName, 'ingredientTypeName', event.target.value);
+  };
+
   return (
     <Grid container direction='row'>
       <Grid item lg={2}>
-        {ingredientRange.ingredientTypeName}
+        <ShadowedSelect value={ingredientRange.ingredientTypeName} onChange={handleChangeIngredientTypeName}>
+          {allIngredientTypes ? allIngredientTypes.map(ingredientType => (
+            <MenuItem value={ingredientType.name}>{ingredientType.name}</MenuItem>
+          )) : null}
+        </ShadowedSelect>
       </Grid>
-      <Grid item lg={1}>
-        <CenteredBody dark>from</CenteredBody>
+      <Grid item lg={1} container direction='column' justify='center'>
+        <Grid item>
+          <CenteredBody dark>from</CenteredBody>
+        </Grid>
       </Grid>
       <Grid item lg={2}>
         {ingredientRange.minimum ? ingredientRange.minimum.amount : 'none'}
@@ -45,4 +85,4 @@ const IngredientRangeSelection: SFC<IProps> = ({ingredientRange}) => {
   )
 };
 
-export default IngredientRangeSelection;
+export default withReferenceData(IngredientRangeSelection);
--- baribuilder-ui/src/typings/gql/AddProduct.ts
+++ baribuilder-ui/src/typings/gql/AddProduct.ts
@@ -7,9 +7,29 @@ import { FREQUENCY, PRODUCT_QUANTITY_UNITS } from "./globalTypes";
 // GraphQL mutation operation: AddProduct
 // ====================================================
 
-export interface AddProduct_AddProductToCurrentRegimen {
+export interface AddProduct_AddProductToCurrentRegimen_products_quantity {
+  __typename: "RegimenProductQuantity";
+  amount: number;
+  units: PRODUCT_QUANTITY_UNITS;
+  frequency: FREQUENCY;
+}
+
+export interface AddProduct_AddProductToCurrentRegimen_products_cost {
+  __typename: "RegimenProductCost";
+  money: number;
+  frequency: FREQUENCY;
+}
+
+export interface AddProduct_AddProductToCurrentRegimen_products {
   __typename: "RegimenProduct";
   catalogProductId: string;
+  quantity: AddProduct_AddProductToCurrentRegimen_products_quantity;
+  cost: AddProduct_AddProductToCurrentRegimen_products_cost;
+}
+
+export interface AddProduct_AddProductToCurrentRegimen {
+  __typename: "Regimen";
+  products: AddProduct_AddProductToCurrentRegimen_products[];
 }
 
 export interface AddProduct {
