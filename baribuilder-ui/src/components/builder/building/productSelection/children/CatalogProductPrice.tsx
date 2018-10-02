96a8f1a59b430934e6ce174ab21c805d9b72bfef
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-10-02 06:29:25 -0500

    showing number of products

--- baribuilder-ui/src/components/builder/building/productSelection/children/CatalogProductPrice.tsx
+++ baribuilder-ui/src/components/builder/building/productSelection/children/CatalogProductPrice.tsx
@@ -1,4 +1,4 @@
-import {Grid, Typography} from '@material-ui/core';
+import {Grid} from '@material-ui/core';
 import gql from 'graphql-tag';
 import * as React from 'react';
 import {Fragment, SFC} from 'react';
@@ -9,12 +9,14 @@ import {
   GetClientCatalogProductPrices,
   GetClientCatalogProductPricesVariables
 } from '../../../../../typings/gql/GetClientCatalogProductPrices';
+import {CenteredTextGrid} from '../../../../goals/GoalsScreenPure';
+import {Header2, Subcaption} from '../../../../style/Typography';
 
 const GET_CLIENT_CATALOG_PRODUCT_PRICES_QUERY = gql`
     query GetClientCatalogProductPrices($catalogProductId: ID!) {
         ClientCatalogProduct(catalogProductId: $catalogProductId) @client {
             catalogProductId # ensure cache hit
-            
+
             cost {
                 money
                 frequency
@@ -45,16 +47,8 @@ const enhance = compose<IProps & DataOutputProps, IProps>(
   pure,
 );
 
-const PriceText = styled(Typography)`
-  && { 
-    font-weight: 600;
-    font-size: 36px;
-    text-align: center;
-  }  
-`;
-
-const PriceDescriptionText = styled(Typography)`
-  text-align: center;
+const SubcaptionBold = styled(Subcaption)`
+  font-weight: bold;
 `;
 
 // Pure
@@ -62,15 +56,29 @@ const CatalogProductPricePure: SFC<IProps & DataOutputProps> = ({data: {ClientCa
   if (!ClientCatalogProduct) {
     return null;
   }
+  const headerText = ClientCatalogProduct.projectedRegimenCost ? 'If you add this product...' : null;
   const price = ClientCatalogProduct.projectedRegimenCost ? ClientCatalogProduct.projectedRegimenCost.money.toFixed(0) : ClientCatalogProduct.cost.money.toFixed(2);
   const subText = ClientCatalogProduct.projectedRegimenCost ? 'Monthly Regimen Cost (Projected)' : 'Monthly Product Cost';
+  const subSubText = ClientCatalogProduct.projectedRegimenCost ? `(with ${ClientCatalogProduct.projectedRegimenCost.numRemainingProducts} more products)` : null;
   return (
     <Fragment>
       <Grid container alignItems='center'>
-        <Grid item lg={12}>
-          <PriceText>${price}</PriceText>
-          <PriceDescriptionText variant='caption'>{subText}</PriceDescriptionText>
-        </Grid>
+        {
+          headerText
+            ? <CenteredTextGrid item lg={12}><Subcaption dark>{headerText}</Subcaption></CenteredTextGrid>
+            : null
+        }
+        <CenteredTextGrid item lg={12}>
+          <Header2 dark>${price}</Header2>
+        </CenteredTextGrid>
+        <CenteredTextGrid item lg={12}>
+          <Subcaption dark>{subText}</Subcaption>
+        </CenteredTextGrid>
+        {
+          subSubText
+            ? <CenteredTextGrid item lg={12}><SubcaptionBold dark>{subSubText}</SubcaptionBold></CenteredTextGrid>
+            : null
+        }
       </Grid>
     </Fragment>
   );
