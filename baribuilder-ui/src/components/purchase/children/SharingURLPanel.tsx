693d2ebbf85c7e5621460cee6e7abe5f774bbcea
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-10-16 05:36:24 -0700

    pulling desired data

--- baribuilder-ui/src/components/purchase/children/SharingURLPanel.tsx
+++ baribuilder-ui/src/components/purchase/children/SharingURLPanel.tsx
@@ -1,15 +1,54 @@
 import {Button, Grid, TextField} from '@material-ui/core';
+import gql from 'graphql-tag';
 import * as React from 'react';
 import {Fragment, SFC} from 'react';
+import {ChildDataProps, graphql} from 'react-apollo';
+import {compose} from 'recompose';
 import styled from 'styled-components';
+import {GetStoreToShare} from '../../../typings/gql/GetStoreToShare';
 import {BoldBody} from '../../style/Typography';
+
+const STORE_TO_SHARE_QUERY = gql`
+    query GetStoreToShare {
+        goalIngredients @client {
+            ingredientRanges {
+                ingredientTypeName
+                minimumAmount
+                maximumAmount
+                units
+                frequency
+            }
+        }
+        currentRegimen @client {
+            products {
+                catalogProductId
+                quantity {
+                    amount
+                    frequency
+                    units
+                }
+                cost {
+                    money
+                    frequency
+                }
+            }
+        }
+    }
+`;
+
+type QueryOutputProps = ChildDataProps<{}, GetStoreToShare>;
+
+// TODO helper funcs for wrangling url params
+
+// TODO toaster for successful copy
 
 const HorizontalPaddedGrid = styled(Grid)`
   padding-left: 8px;
   padding-right: 8px;
 `;
 
-const SharingURLPanel: SFC = () => {
+const SharingURLPanel: SFC<QueryOutputProps> = ({data: {currentRegimen, goalIngredients}}) => {
+  // TODO copy functionality
   return (
     <Fragment>
       <HorizontalPaddedGrid item container lg={10}>
@@ -29,4 +68,10 @@ const SharingURLPanel: SFC = () => {
   )
 };
 
-export default SharingURLPanel;
+const withData = graphql<{}, GetStoreToShare>(STORE_TO_SHARE_QUERY);
+
+const enhance = compose<QueryOutputProps, {}>(
+  withData
+);
+
+export default enhance(SharingURLPanel);
