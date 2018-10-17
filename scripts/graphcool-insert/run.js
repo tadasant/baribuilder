b68bd4b428d5caccc4244d7704def6b07cb4040d
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-10-17 12:49:44 -0700

    fixed up the categories ordering and naming

--- scripts/graphcool-insert/run.js
+++ scripts/graphcool-insert/run.js
@@ -1,5 +1,5 @@
 import {fromEvent} from 'graphcool-lib';
-import data from './input/imageURLs';
+import data from './input/productCategories';
 
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzgzOTA3NjAsImNsaWVudElkIjoiY2psemtzaHB0MTB0czAxNDIybDlzM2pkbyIsInByb2plY3RJZCI6ImNqbHpxdmF3dDFpYjAwMTA3ZzNuZnIwNGkiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqbXE2NHptajJ5bGcwMTMwbzFsenhjbGMifQ.17q8GNMZI8r53I3QfgQhlkumnbZQRezevQ_L7NE0pHA';
 const projectId = 'cjlzqvawt1ib00107g3nfr04i';
@@ -58,7 +58,7 @@ const ingredientTypeRecordToEntry = (record) => {
 const catalogProductRecordToEntry = (record) => {
   return {
     'id': `"${record['id']}"`, // string
-    'name': `"${record['name']}"`, // string
+    'category': `${record['category']}`, // enum
   }
 };
 
@@ -84,25 +84,25 @@ const imageURLRecordToEntry = (record) => {
 // };
 
 // updating product names
-// const run = () => {
-//   data.forEach(record => {
-//     const entry = catalogProductRecordToEntry(record);
-//     const query = generateUpdateMutation('CatalogProduct', entry);
-//     api
-//       .request(query)
-//       .then(data => console.log(data))
-//   });
-// };
-
 const run = () => {
   data.forEach(record => {
-    const entry = imageURLRecordToEntry(record);
-    const query = generateCreateMutation('Image', entry);
-    // console.log(query);
+    const entry = catalogProductRecordToEntry(record);
+    const query = generateUpdateMutation('CatalogProduct', entry);
     api
       .request(query)
       .then(data => console.log(data))
   });
 };
 
-run();
+// const run = () => {
+//   data.forEach(record => {
+//     const entry = imageURLRecordToEntry(record);
+//     const query = generateCreateMutation('Image', entry);
+//     // console.log(query);
+//     api
+//       .request(query)
+//       .then(data => console.log(data))
+//   });
+// };
+
+// run();
