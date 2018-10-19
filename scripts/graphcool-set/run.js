67e577867786347d194c74832f8649ee863f6398
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-10-19 16:36:07 -0700

    updated permissions on new model

--- scripts/graphcool-set/run.js
+++ scripts/graphcool-set/run.js
@@ -2,25 +2,25 @@ import { GraphQLClient } from 'graphql-request'
 
 const client = new GraphQLClient('https://api.graph.cool/simple/v1/cjlzqvawt1ib00107g3nfr04i', {
   headers: {
-    Authorization: 'Bearer YOUR_AUTH_TOKEN',
+    Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzgzOTA3NjAsImNsaWVudElkIjoiY2psemtzaHB0MTB0czAxNDIybDlzM2pkbyIsInByb2plY3RJZCI6ImNqbHpxdmF3dDFpYjAwMTA3ZzNuZnIwNGkiLCJwZXJtYW5lbnRBdXRoVG9rZW5JZCI6ImNqbXE2NHptajJ5bGcwMTMwbzFsenhjbGMifQ.17q8GNMZI8r53I3QfgQhlkumnbZQRezevQ_L7NE0pHA',
   },
 });
 
-const getNutritionFactses = () => {
+const getPackageListings = () => {
   return client.request(`
     {
-      allNutritionFactses {
+      allPackageListings {
         id
-        serving {
-          size
-          units
+        numServings
+        product {
+          id
         }
       }
     }
   `);
 };
 
-const setServingStuff = (id, size, units) => {
+const createProductPackage = (listingId, numServings, productId) => {
   return client.request(`
     mutation {
       updateNutritionFacts(
@@ -34,13 +34,13 @@ const setServingStuff = (id, size, units) => {
   `);
 };
 
-// getNutritionFactses()
-//   .then(data => {
-//     const nutritionFactses = data.allNutritionFactses;
-//     nutritionFactses.forEach(nutritionFacts => {
-//       const {id} = nutritionFacts;
-//       const {size, units} = nutritionFacts['serving'];
-//       setServingStuff(id, size, units).then(data => console.log(data));
-//     });
-//   })
-//   .catch(error => console.log(error));
+getNutritionFactses()
+  .then(data => {
+    const nutritionFactses = data.allNutritionFactses;
+    nutritionFactses.forEach(nutritionFacts => {
+      const {id} = nutritionFacts;
+      const {size, units} = nutritionFacts['serving'];
+      setServingStuff(id, size, units).then(data => console.log(data));
+    });
+  })
+  .catch(error => console.log(error));
