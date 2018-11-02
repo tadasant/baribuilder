d03262953e8037347fd701722fe48e21cd19e224
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-02 12:39:04 -0700

    clearability for products done

--- baribuilder-ui/src/components/catalog/children/BuilderMyProducts.tsx
+++ baribuilder-ui/src/components/catalog/children/BuilderMyProducts.tsx
@@ -1,11 +1,28 @@
 import {Button, Grid} from '@material-ui/core';
+import gql from 'graphql-tag';
 import * as React from 'react';
 import {Fragment, SFC} from 'react';
+import {DataProps, graphql, MutateProps} from 'react-apollo';
+import {toast} from 'react-toastify';
 import styled from 'styled-components';
 import Sketch from '../../../app/style/SketchVariables';
+import {ClearCurrentRegimen} from '../../../typings/gql/ClearCurrentRegimen';
 import {EmptyRow} from '../../style/Layout';
 import {ButtonFooter, ButtonFooterGrid, HPaddedGrid, NoMarginGrid} from './BuilderMyRegimen';
 import CurrentRegimenProducts from './myProducts/CurrentRegimenProducts';
+
+
+const REGIMEN_CLEAR_MUTATION = gql`
+    mutation ClearCurrentRegimen {
+        ClearCurrentRegimen @client {
+            products {
+                catalogProductId
+            }
+        }
+    }
+`;
+
+type MutationOutputProps = Partial<DataProps<ClearCurrentRegimen>> & Partial<MutateProps<ClearCurrentRegimen>>;
 
 const PaddedGrid = styled(Grid)`
   padding-left: 8px;
@@ -26,7 +43,20 @@ const RedButton = styled(Button)`
   }
 `;
 
-const BuilderMyProducts: SFC = () => {
+const BuilderMyProducts: SFC<MutationOutputProps> = ({mutate}) => {
+  const handleClearClick = () => {
+    if (!mutate) {
+      console.error('Mutate undefined for some reason. Error code 019472');
+      return;
+    }
+    mutate()
+      .then(response => {
+        if (response && !response.errors) {
+          toast.success('Cleared all products.');
+        }
+      })
+  };
+
   return (
     <Fragment>
       <PaddedGrid container alignContent='flex-start'>
@@ -38,7 +68,7 @@ const BuilderMyProducts: SFC = () => {
         <ButtonFooterGrid item lg={12} container direction='column' justify='center'>
           <NoMarginGrid item container>
             <HPaddedGrid item lg={12}>
-              <RedButton variant='raised' fullWidth>
+              <RedButton variant='raised' fullWidth onClick={handleClearClick}>
                 Clear All
               </RedButton>
             </HPaddedGrid>
@@ -49,4 +79,6 @@ const BuilderMyProducts: SFC = () => {
   )
 };
 
-export default BuilderMyProducts;
+const withMutation = graphql<{}, ClearCurrentRegimen>(REGIMEN_CLEAR_MUTATION);
+
+export default withMutation(BuilderMyProducts);
