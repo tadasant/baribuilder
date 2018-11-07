3d2e0d798a0e4d76f018236cbf386f83e05737db
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-07 06:25:00 -0800

    disable mobile splash screen

--- baribuilder-ui/src/app/BuilderAppPure.tsx
+++ baribuilder-ui/src/app/BuilderAppPure.tsx
@@ -1,16 +1,12 @@
-import {Grid, Hidden} from '@material-ui/core';
 import * as React from 'react';
 import {Fragment, SFC} from 'react';
 import {Route, RouteComponentProps, Switch, withRouter} from 'react-router-dom';
 import CatalogScreen from '../components/catalog/CatalogScreen';
 import Footer from '../components/Footer';
 import GoalsScreen from '../components/goals/GoalsScreenContainer';
-import {CenteredTextGrid} from '../components/goals/GoalsScreenPure';
 import PurchaseScreen from '../components/purchase/PurchaseScreen';
 import RequestProductsScreen from '../components/request/RequestProductsScreen';
 import ShareScreen from '../components/share/ShareScreen';
-import {EmptyRow} from '../components/style/Layout';
-import {Header} from '../components/style/Typography';
 import TermsAndConditions from '../components/TermsAndConditions';
 import {isProduction} from '../config/config';
 import Dev from './Dev';
@@ -23,33 +19,18 @@ const disclaimerText = 'The information on this website is not medical advice. P
 const BuilderAppPure: SFC<RouteComponentProps> = ({location}) => {
   return (
     <Fragment>
-      <Hidden mdDown>
-        <Switch key={location.pathname}>
-          <Route path="/browse" component={CatalogScreen}/>
-          <Route exact path="/goals" component={GoalsScreen}/>
-          <Route exact path="/purchase" component={PurchaseScreen}/>
-          <Route exact path="/share" component={ShareScreen}/>
-          <Route exact path="/request-products" component={RequestProductsScreen}/>
-          <Route exact path="/terms-and-conditions" component={TermsAndConditions}/>
-          {isProduction ? null : <Route exact path='/dev' component={Dev}/>}
-          <Route component={NotFound}/>
-        </Switch>
-        <Footer disclaimerText={disclaimerText}/>
-      </Hidden>
-      <Hidden lgUp>
-        <Grid container>
-          <EmptyRow/>
-          <Grid item xs={1}/>
-          <CenteredTextGrid item xs={10}>
-            <Header dark>
-              Sorry, BariBuilder Beta is not yet available on smaller screens.<br/><br/>
-
-              Please visit this page on a desktop with a screen at least 1000 pixels wide.
-            </Header>
-          </CenteredTextGrid>
-          <Grid item xs={1}/>
-        </Grid>
-      </Hidden>
+      {/* key needed to force re-mount on screen change --> natural e-commerce UX*/}
+      <Switch key={location.pathname}>
+        <Route path="/browse" component={CatalogScreen}/>
+        <Route exact path="/goals" component={GoalsScreen}/>
+        <Route exact path="/purchase" component={PurchaseScreen}/>
+        <Route exact path="/share" component={ShareScreen}/>
+        <Route exact path="/request-products" component={RequestProductsScreen}/>
+        <Route exact path="/terms-and-conditions" component={TermsAndConditions}/>
+        {isProduction ? null : <Route exact path='/dev' component={Dev}/>}
+        <Route component={NotFound}/>
+      </Switch>
+      <Footer disclaimerText={disclaimerText}/>
     </Fragment>
   );
 };
