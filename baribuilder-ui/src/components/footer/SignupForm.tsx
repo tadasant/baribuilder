688a2e567c61df4f984f737b44b86eff90575768
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-30 14:17:20 -0800

    last typo fixes

--- baribuilder-ui/src/components/footer/SignupForm.tsx
+++ baribuilder-ui/src/components/footer/SignupForm.tsx
@@ -52,7 +52,7 @@ class SignupForm extends Component {
       <Fragment>
         <CenteredTextGrid item xs={12}>
           <Body>
-          Subscribe to BariBuilder product announcements & updates
+          Subscribe to BariBuilder app announcements & updates
           </Body>
         </CenteredTextGrid>
         <EmptyRow/>
