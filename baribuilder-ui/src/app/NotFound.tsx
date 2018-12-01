66ef450444929381227b3dd1ba3c4e91bc742deb
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-12-01 10:47:10 -0800

    404 helpful error

--- baribuilder-ui/src/app/NotFound.tsx
+++ baribuilder-ui/src/app/NotFound.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {SFC} from 'react';
 
-const NotFound: SFC = () => <h1>404 Not Found</h1>;
+const NotFound: SFC = () => <h1>404 Not Found.<br/><br/> If you followed a valid link, try clearing your browser cache and try again. Shortcut: <br/>(MacOS) CMD + SHIFT + R <br/>(Windows) CTRL + F5</h1>;
 
 export default NotFound;
