940d080b1024db72161cc1ea115aa4ea9acf30ea
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2019-06-08 10:43:25 -0700

    adjust for shop.baribuilder.com

--- baribuilder-ui/src/config/config.ts
+++ baribuilder-ui/src/config/config.ts
@@ -2,6 +2,6 @@ import {IConfig} from './configTyping';
 import devConfig from './dev.config';
 import prodConfig from './prod.config';
 
-export const isProduction = window.location.host === 'baribuilder.com';
+export const isProduction = window.location.host === 'baribuilder.com' || window.location.host === 'shop.baribuilder.com';
 const config: IConfig = isProduction ? prodConfig : devConfig;
 export default config;
