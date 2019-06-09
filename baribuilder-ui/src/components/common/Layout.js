423e9f8a29c25a885679bc9bf4f069e551014b5a
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2019-06-09 13:02:37 -0700

    offpage logo setup

--- baribuilder-ui/src/components/common/Layout.js
+++ baribuilder-ui/src/components/common/Layout.js
@@ -1,106 +1,169 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { Link, StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
-import { Navigation } from '.'
-import config from '../../utils/siteConfig'
+import { Navigation } from ".";
+import config from "../../utils/siteConfig";
 
 // Styles
-import '../../styles/app.css'
+import "../../styles/app.css";
 
 /**
-* Main layout component
-*
-* The Layout component wraps around each page and template.
-* It also provides the header, footer as well as the main
-* styles, and meta data for each page.
-*
-*/
-const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
-    const site = data.allGhostSettings.edges[0].node
-    const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
-    const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
+ * Main layout component
+ *
+ * The Layout component wraps around each page and template.
+ * It also provides the header, footer as well as the main
+ * styles, and meta data for each page.
+ *
+ */
+const DefaultLayout = props => {
+    const { data, children, bodyClass, isHome } = props;
+    const site = data.allGhostSettings.edges[0].node;
+    const twitterUrl = site.twitter
+        ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}`
+        : null;
+    const facebookUrl = site.facebook
+        ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}`
+        : null;
 
     return (
-    <>
-        <Helmet>
-            <html lang={site.lang} />
-            <style type="text/css">{`${site.codeinjection_styles}`}</style>
-            <body className={bodyClass} />
-        </Helmet>
+        <>
+            <Helmet>
+                <html lang={site.lang} />
+                <style type="text/css">{`${site.codeinjection_styles}`}</style>
+                <body className={bodyClass} />
+            </Helmet>
 
-        <div className="viewport">
-
-            <div className="viewport-top">
-                {/* The main header section on top of the screen */}
-                <header className="site-head">
-                    <div className="container">
-                        { isHome ?
-                            <div className="site-banner">
-                                <Link to="/">
-                                    {site.logo ?
-                                        <img className="site-logo" src={site.logo} alt={site.title} />
-                                        : "BariBuilder"
-                                    }
-                                </Link>
-                                <p className="site-banner-desc">{site.description}</p>
-                            </div> :
-                            null}
-                        <nav className="site-nav">
-                            <div className="site-nav-left">
-                                {/* The navigation items as setup in Ghost */}
-                                <Navigation data={site.navigation} navClass="site-nav-item" />
-                            </div>
-                            <div className="site-nav-right">
-                            { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
-                                <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
-                            </div>
-                        </nav>
-                    </div>
-                </header>
-
-                <main className="site-main">
-                    {/* All the main content gets inserted here, index.js, post.js */}
-                    {children}
-                </main>
-
-            </div>
-
-            <div className="viewport-bottom">
-                {/* The footer at the very bottom of the screen */}
-                <footer className="site-foot">
-                    <div className="site-foot-nav container">
-                        <div className="site-foot-nav-left">
-                            <Link to="/">{site.title}</Link> © 2019
+            <div className="viewport">
+                <div className="viewport-top">
+                    {/* The main header section on top of the screen */}
+                    <header className="site-head">
+                        <div className="container">
+                            {isHome ? (
+                                <div className="site-banner">
+                                    <Link to="/">
+                                        {site.logo ? (
+                                            <img
+                                                className="site-logo"
+                                                src={site.logo}
+                                                alt={site.title}
+                                            />
+                                        ) : (
+                                            "BariBuilder"
+                                        )}
+                                    </Link>
+                                    <p className="site-banner-desc">
+                                        {site.description}
+                                    </p>
+                                </div>
+                            ) : null}
+                            <nav className="site-nav">
+                                <div className="site-nav-left">
+                                    {!isHome ? (
+                                        <Link to="/" className="site-nav-logo">
+                                            {data.logo ? (
+                                                <Img fixed={data.logo.childImageSharp.fixed} alt={site.title} />
+                                            ) : (
+                                                "BariBuilder"
+                                            )}
+                                        </Link>
+                                    ) : null}
+                                    {/* The navigation items as setup in Ghost */}
+                                    <Navigation
+                                        data={site.navigation}
+                                        navClass="site-nav-item"
+                                    />
+                                </div>
+                                <div className="site-nav-right">
+                                    {site.twitter && (
+                                        <a
+                                            href={twitterUrl}
+                                            className="site-nav-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <img
+                                                className="site-nav-icon"
+                                                src="/images/icons/twitter.svg"
+                                                alt="Twitter"
+                                            />
+                                        </a>
+                                    )}
+                                    {site.facebook && (
+                                        <a
+                                            href={facebookUrl}
+                                            className="site-nav-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <img
+                                                className="site-nav-icon"
+                                                src="/images/icons/facebook.svg"
+                                                alt="Facebook"
+                                            />
+                                        </a>
+                                    )}
+                                    <a
+                                        className="site-nav-item"
+                                        href={`https://feedly.com/i/subscription/feed/${
+                                            config.siteUrl
+                                        }/rss/`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img
+                                            className="site-nav-icon"
+                                            src="/images/icons/rss.svg"
+                                            alt="RSS Feed"
+                                        />
+                                    </a>
+                                </div>
+                            </nav>
                         </div>
-                        <div className="site-foot-nav-right">
-                            <Navigation data={site.navigation} navClass="site-foot-nav-item" />
+                    </header>
+
+                    <main className="site-main">
+                        {/* All the main content gets inserted here, index.js, post.js */}
+                        {children}
+                    </main>
+                </div>
+
+                <div className="viewport-bottom">
+                    {/* The footer at the very bottom of the screen */}
+                    <footer className="site-foot">
+                        <div className="site-foot-nav container">
+                            <div className="site-foot-nav-left">
+                                <Link to="/">{site.title}</Link> © 2019
+                            </div>
+                            <div className="site-foot-nav-right">
+                                <Navigation
+                                    data={site.navigation}
+                                    navClass="site-foot-nav-item"
+                                />
+                            </div>
                         </div>
-                    </div>
-                </footer>
-
+                    </footer>
+                </div>
             </div>
-        </div>
-
-    </>
-    )
-}
+        </>
+    );
+};
 
 DefaultLayout.propTypes = {
     children: PropTypes.node.isRequired,
     bodyClass: PropTypes.string,
     isHome: PropTypes.bool,
     data: PropTypes.shape({
-        allGhostSettings: PropTypes.object.isRequired,
-    }).isRequired,
-}
+        allGhostSettings: PropTypes.object.isRequired
+    }).isRequired
+};
 
 const DefaultLayoutSettingsQuery = props => (
     <StaticQuery
         query={graphql`
-            query GhostSettings {
+            query LayoutQuery {
                 allGhostSettings {
                     edges {
                         node {
@@ -108,10 +171,17 @@ const DefaultLayoutSettingsQuery = props => (
                         }
                     }
                 }
+                logo: file(relativePath: { eq: "baribuilder-logo-white.png" }) {
+                    childImageSharp {
+                        fixed(height: 30) {
+                            ...GatsbyImageSharpFixed
+                        }
+                    }
+                }
             }
         `}
         render={data => <DefaultLayout data={data} {...props} />}
     />
-)
+);
 
-export default DefaultLayoutSettingsQuery
+export default DefaultLayoutSettingsQuery;
