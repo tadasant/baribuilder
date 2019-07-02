7c83e3b558f9faff6f32116dfde85f3539c35597
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2019-07-02 05:36:11 -0700

    update reviewers

--- baribuilder-ui/src/utils/medicallyReviewed.js
+++ baribuilder-ui/src/utils/medicallyReviewed.js
@@ -1,4 +1,5 @@
 // Post slugs exported here will be displayed as medically reviewed by Dr. G
 module.exports = {
-	"gastric-bypass-dumping-syndrome": ["gintas"]
+	"gastric-bypass-dumping-syndrome": ["gintas"],
+	"gastric-bypass-pre-op-diet": ["gintas"]
 };
