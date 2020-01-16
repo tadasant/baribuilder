5cff59db9c9dbf09803ec2d62bfccf934776522e
kamieallen <55499323+kamieallen@users.noreply.github.com>
2020-01-16 14:41:02 -0500

    Updated

--- baribuilder-ui/src/utils/medicallyReviewed.js
+++ baribuilder-ui/src/utils/medicallyReviewed.js
@@ -39,5 +39,6 @@ module.exports = {
 	"gastric-bypass-and-alcohol": ["gintas"],
 	"gastric-sleeve-complications": ["gintas"],
 	"cheating-on-liquid-diet-before-gastric-bypass": ["gintas"],
-	"gastric-sleeve-pouch-reset": ["gintas"]
+	"gastric-sleeve-pouch-reset": ["gintas"],
+	"dumping-syndrome-gastric-sleeve": ["gintas"]
 };
