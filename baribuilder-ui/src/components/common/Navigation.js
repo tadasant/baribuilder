36e701be473abd30c4f0cf96fa87523fd20bea04
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2020-04-07 05:32:06 -0700

    mobile nav

--- baribuilder-ui/src/components/common/Navigation.js
+++ baribuilder-ui/src/components/common/Navigation.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
-import { Menu, MenuItem } from "@material-ui/core";
+import { Menu, MenuItem, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import upperFirst from "lodash/upperFirst";
 
 /**
@@ -16,64 +17,141 @@ import upperFirst from "lodash/upperFirst";
  */
 const Navigation = ({ navClass, tags }) => {
 	const [topicsMenuAnchor, setTopicsMenuAnchor] = useState(null);
+	const [hamburgerMenuAnchor, setHamburgerMenuAnchor] = useState(null);
 	const handleTopicsClick = (event) => {
 		setTopicsMenuAnchor((prev) => (prev === null ? event.currentTarget : null));
+	};
+	const handleHamburgerClick = (event) => {
+		setHamburgerMenuAnchor((prev) =>
+			prev === null ? event.currentTarget : null
+		);
 	};
 
 	const sortedTags = tags
 		.sort((a, b) => (a.postCount > b.postCount ? -1 : 1))
 		.slice(0, 8);
 
+	const navItems = [
+		{
+			url: "/blog/",
+			label: "Blog",
+			isRelative: true,
+			hasSubMenu: false,
+		},
+		{
+			label: "Topics",
+			hasSubMenu: true,
+		},
+		{
+			url: "https://shop.baribuilder.com/",
+			label: "Shop",
+			isAbsolute: true,
+		},
+		{
+			url: "https://shop.baribuilder.com/about",
+			label: "About",
+			isAbsolute: true,
+		},
+	];
+
 	return (
 		<>
-			<Link className={navClass} to="/blog/">
-				Blog
-			</Link>
-			<span
-				className={navClass}
-				aria-controls="simple-menu"
-				aria-haspopup="true"
-				onClick={handleTopicsClick}
-			>
-				Topics
-			</span>
-			<Menu
-				anchorEl={topicsMenuAnchor}
-				keepMounted
-				open={Boolean(topicsMenuAnchor)}
-				onClose={handleTopicsClick}
-				anchorOrigin={{
-					vertical: "bottom",
-				}}
-				getContentAnchorEl={null}
-				className="site-nav-sub-menu"
-			>
-				{sortedTags.map((tag, i) => (
-					<MenuItem>
-						<Link
-							className="site-nav-sub-menu-item"
-							to={`/blog/tag/${tag.slug}`}
-							key={i}
-						>
-							{upperFirst(tag.name)} ({tag.postCount})
-						</Link>
-					</MenuItem>
-				))}
-			</Menu>
-			<a
-				className={navClass}
-				href="https://shop.baribuilder.com/"
-				rel="noopener noreferrer"
-			>
-				Shop
-			</a>
-			<a
-				className={navClass}
-				href="https://shop.baribuilder.com/about"
-				rel="noopener noreferrer"
-			>
-				About
-			</a>
+			{/* Mobile first */}
+			<div className="hamburger-menu">
+				<IconButton
+					aria-label="menu"
+					className="hamburger-menu"
+					onClick={handleHamburgerClick}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Menu
+					anchorEl={hamburgerMenuAnchor}
+					keepMounted
+					open={Boolean(hamburgerMenuAnchor)}
+					onClose={handleHamburgerClick}
+					anchorOrigin={{
+						horizontal: "right",
+					}}
+					getContentAnchorEl={null}
+					className="site-nav-sub-menu"
+				>
+					{sortedTags.map((tag, i) => (
+						<MenuItem>
+							<Link
+								className="site-nav-sub-menu-item"
+								to={`/blog/tag/${tag.slug}`}
+								key={i}
+							>
+								{upperFirst(tag.name)} ({tag.postCount})
+							</Link>
+						</MenuItem>
+					))}
+				</Menu>
+			</div>
+
+			{/* Desktop below */}
+			<div className="flat-menu">
+				{navItems.map((navItem) => {
+					if (navItem.isRelative) {
+						return (
+							<Link className={navClass} to={navItem.url}>
+								{navItem.label}
+							</Link>
+						);
+					}
+
+					if (navItem.isAbsolute) {
+						return (
+							<a
+								className={navClass}
+								href={navItem.url}
+								rel="noopener noreferrer"
+							>
+								{navItem.label}
+							</a>
+						);
+					}
+
+					if (navItem.hasSubMenu) {
+						return (
+							<>
+								<span
+									className={navClass}
+									aria-controls="simple-menu"
+									aria-haspopup="true"
+									onClick={handleTopicsClick}
+								>
+									{navItem.label}
+								</span>
+								<Menu
+									anchorEl={topicsMenuAnchor}
+									keepMounted
+									open={Boolean(topicsMenuAnchor)}
+									onClose={handleTopicsClick}
+									anchorOrigin={{
+										vertical: "bottom",
+									}}
+									getContentAnchorEl={null}
+									className="site-nav-sub-menu"
+								>
+									{sortedTags.map((tag, i) => (
+										<MenuItem>
+											<Link
+												className="site-nav-sub-menu-item"
+												to={`/blog/tag/${tag.slug}`}
+												key={i}
+											>
+												{upperFirst(tag.name)} ({tag.postCount})
+											</Link>
+										</MenuItem>
+									))}
+								</Menu>
+							</>
+						);
+					}
+				})}
+			</div>
 		</>
 	);
 };
