54ca280e4bc2818f324773a50b9282a76ccf76e6
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2020-07-11 21:37:01 -0400

    added randomization

--- baribuilder-ui/src/components/ads/AffiliateSliderContainer.js
+++ baribuilder-ui/src/components/ads/AffiliateSliderContainer.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AffiliateSlider from "./AffiliateSlider";
 import affiliateAds from "../../utils/affiliateAds";
 import PropTypes from "prop-types";
 
 const AffiliateSliderContainer = (props) => {
+	const [chosenAd, setChosenAd] = useState(null);
+
 	const { slug } = props;
 
-	// TODO Choose a randomized ad from the matching list
-	const chosenAd =
-		slug in affiliateAds ? affiliateAds[slug][0] : affiliateAds.default[0];
-	return <AffiliateSlider ad={chosenAd} {...props} />;
+	useEffect(() => {
+		const candidateAds =
+			slug in affiliateAds ? affiliateAds[slug] : affiliateAds.default;
+		const randomIdx = Math.floor(Math.random() * candidateAds.length);
+		setChosenAd(candidateAds[randomIdx]);
+	}, [slug]);
+
+	return chosenAd ? <AffiliateSlider ad={chosenAd} {...props} /> : null;
 };
 
 AffiliateSliderContainer.propTypes = {
