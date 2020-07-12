1f33da784eb50bf8909d7cd683f67787f9a7fa92
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2020-07-11 23:44:16 -0400

    template top 10 config

--- baribuilder-ui/src/utils/affiliateAds.js
+++ baribuilder-ui/src/utils/affiliateAds.js
@@ -11,26 +11,157 @@
  * }
  */
 
-const adsById = {
-	"bariatric-advantage-b12-hair": {
-		id: "bariatric-advantage-b12",
-		link: "https://amzn.to/323j7B0",
-		copy: "Bariatric B12 Vitamins can help prevent hair loss",
-		imgSrc:
-			"https://baribuilder-affiliate-imgs.s3.amazonaws.com/bariatric-advantage-b12.png",
-	},
-	"barimelts-calcium-default": {
-		id: "barimelts-calcium",
-		link: "https://amzn.to/3gMSCnt",
-		copy: "Use Bariatric Calcium to keep your bones strong",
-		imgSrc:
-			"https://baribuilder-affiliate-imgs.s3.amazonaws.com/bari-melts-calcium.png",
-	},
+const proteinShakesAd = {
+	link: "https://amzn.to/3flPK0l",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/premier-protein.png",
+};
+
+const proteinInfusedWaterAd = {
+	link: "https://amzn.to/2WdVFNM",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/protein-water.png",
+};
+
+const crystalLightAd = {
+	link: "https://amzn.to/2OfW5ij",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/crystal-light.png",
+};
+
+const multivitaminAd = {
+	link: "https://amzn.to/2Of4Ske",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/ba-multivitamin.png",
 };
 
 const affiliateAds = {
-	"gastric-bypass-and-hair-loss": [adsById["bariatric-advantage-b12-hair"]],
-	default: [adsById["barimelts-calcium-default"]],
+	"gastric-bypass-pouch-reset": [
+		{
+			...proteinInfusedWaterAd,
+			id: "protein-infused-water-pouch-reset",
+			copy: "",
+		},
+		{
+			...proteinShakesAd,
+			id: "protein-shakes-pouch-reset",
+			copy: "",
+		},
+		{
+			...crystalLightAd,
+			id: "crystal-light-pouch-reset",
+			copy: "",
+		},
+	],
+	"liver-shrinking-diet": [
+		{
+			...proteinInfusedWaterAd,
+			id: "protein-infused-water-liver-shrinking",
+			copy: "",
+		},
+		{
+			...proteinShakesAd,
+			id: "protein-shakes-liver-shrinking",
+			copy: "",
+		},
+		{
+			...crystalLightAd,
+			id: "crystal-light-liver-shrinking",
+			copy: "",
+		},
+	],
+	"gastric-bypass-and-hair-loss": [
+		{
+			id: "b12-hair-bypass",
+			link: "https://amzn.to/323j7B0",
+			copy: "Bariatric B12 Vitamins can help prevent hair loss",
+			imgSrc:
+				"https://baribuilder-affiliate-imgs.s3.amazonaws.com/bariatric-advantage-b12.png",
+		},
+		{
+			...proteinShakesAd,
+			id: "protein-shakes-hair-bypass",
+			copy: "",
+		},
+	],
+	"cheating-on-liquid-diet-before-gastric-bypass": [
+		{
+			...proteinShakesAd,
+			id: "protein-shakes-liquid-diet-bypass",
+			copy: "",
+		},
+		{
+			...crystalLightAd,
+			id: "crystal-light-liquid-diet-bypass",
+			copy: "",
+		},
+	],
+	"how-to-tighten-loose-skin-after-gastric-bypass-without-surgery": [
+		{
+			...proteinInfusedWaterAd,
+			id: "protein-infused-water-loose-skin-without-surgery",
+			copy: "",
+		},
+	],
+	"liquid-diet-before-gastric-bypass-surgery": [
+		{
+			...proteinInfusedWaterAd,
+			id: "protein-infused-water-liquid-bypass",
+			copy: "",
+		},
+		{
+			...crystalLightAd,
+			id: "crystal-light-liquid-bypass",
+			copy: "",
+		},
+	],
+	"bariatric-surgery-and-popcorn": [
+		{
+			...proteinShakesAd,
+			id: "protein-shakes-popcorn",
+			copy: "",
+		},
+	],
+	"gastric-sleeve-regrets": [
+		{
+			...multivitaminAd,
+			id: "sleeve-regrets-multivitamin",
+			copy: "",
+		},
+	],
+	"gastric-sleeve-and-alcohol": [
+		{
+			...multivitaminAd,
+			id: "sleeve-alcohol-multivitamin",
+			copy: "",
+		},
+	],
+	"gastric-bypass-pre-op-diet": [
+		{
+			...proteinInfusedWaterAd,
+			id: "protein-infused-water-bypass-preop",
+			copy: "",
+		},
+		{
+			...proteinShakesAd,
+			id: "protein-shakes-bypass-preop",
+			copy: "",
+		},
+		{
+			...crystalLightAd,
+			id: "crystal-light-bypass-preop",
+			copy: "",
+		},
+	],
+	default: [
+		{
+			id: "barimelts-calcium",
+			link: "https://amzn.to/3gMSCnt",
+			copy: "Use Bariatric Calcium to keep your bones strong",
+			imgSrc:
+				"https://baribuilder-affiliate-imgs.s3.amazonaws.com/bari-melts-calcium.png",
+		},
+	],
 };
 
 module.exports = affiliateAds;
