b24f5220f665aa99a8be26adcb67c0656e21d375
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2020-10-20 18:12:58 -0700

    set up new ebook ads

--- baribuilder-ui/src/utils/affiliateAds.js
+++ baribuilder-ui/src/utils/affiliateAds.js
@@ -11,6 +11,42 @@
  * }
  */
 
+const bariatricKetoEbookAd = {
+	link: "https://baribuilder.ck.page/4f5e648161",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/keto-diet-aff-img.png",
+};
+
+const backOnTrackEbookAd = {
+	link: "https://baribuilder.ck.page/ca8a1a9973",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/ontrack-mock-aff-img.png",
+};
+
+const bariatricVitaminsEbookAd = {
+	link: "https://baribuilder.ck.page/f6cb07e0ee",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/vitamins-mock-aff-img.png",
+};
+
+const preOpGuideToBariatricsEbookAd = {
+	link: "https://baribuilder.ck.page/058f2745e7",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/preops-mock-aff-img.png",
+};
+
+const bariatricKetoSnacks = {
+	link: "https://baribuilder.ck.page/5599df18b2",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/keto-snacks-aff-img.jpg",
+};
+
+const bariatricKetoCourse = {
+	link: "https://baribuilder.ck.page/0edbb141e9",
+	imgSrc:
+		"https://baribuilder-affiliate-imgs.s3.amazonaws.com/drg-course-aff-img.jpg",
+};
+
 const proteinShakesAd = {
 	link:
 		"https://www.amazon.com/Premier-Protein-Shake-Chocolate-11-5/dp/B07MJL8NXR/ref=as_li_ss_tl?dchild=1&keywords=premier+protein+shakes&qid=1594523391&sr=8-4&linkCode=ll1&tag=bariduiler-ad-20&linkId=a9fc18273fc6a5870c221eb23b8511e5&language=en_US",
@@ -81,274 +117,323 @@ const surgeonAd = {
 // TODO: bariatric meal deliveries? "Put it on autopilot: bariatric meal deliveries"
 
 const affiliateAds = {
-	"gastric-bypass-pouch-reset": [
-		{
-			...proteinInfusedWaterAd,
-			id: "protein-infused-water-pouch-reset",
-			copy: "Get some protein infused water to assist your reset",
-		},
-		{
-			...proteinShakesAd,
-			id: "protein-shakes-pouch-reset",
-			copy: "Get some protein shakes to assist your reset",
-		},
-		{
-			...crystalLightAd,
-			id: "crystal-light-pouch-reset",
-			copy: "Use Crystal Light as a sugar-free, low calorie liquid",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric bypass journey",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-39topics",
-			copy: "39 gastric bypass topics, all in one e-book",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric bypass post-op questions",
-		},
-	],
-	"liver-shrinking-diet": [
-		{
-			...proteinInfusedWaterAd,
-			id: "protein-infused-water-liver-shrinking",
-			copy: "Get some protein infused water as part of the diet",
-		},
-		{
-			...proteinShakesAd,
-			id: "protein-shakes-liver-shrinking",
-			copy: "Get some protein shakes as part of the diet",
-		},
-		{
-			...crystalLightAd,
-			id: "crystal-light-liver-shrinking",
-			copy: "Use Crystal Light as a sugar-free, low calorie liquid",
-		},
-	],
-	"gastric-bypass-and-hair-loss": [
-		{
-			id: "b12-hair-bypass",
-			link: "https://amzn.to/323j7B0",
-			copy: "Bariatric B12 Vitamins can help prevent hair loss",
-			imgSrc:
-				"https://baribuilder-affiliate-imgs.s3.amazonaws.com/bariatric-advantage-b12.png",
-		},
-		{
-			...proteinShakesAd,
-			id: "protein-shakes-hair-bypass",
-			copy: "Make sure you're getting plenty of protein shakes",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric bypass journey",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-39topics",
-			copy: "39 gastric bypass topics, all in one e-book",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric bypass post-op questions",
-		},
-	],
-	"cheating-on-liquid-diet-before-gastric-bypass": [
-		{
-			...proteinShakesAd,
-			id: "protein-shakes-liquid-diet-bypass",
-			copy: "Don't cheat: drink protein shakes",
-		},
-		{
-			...crystalLightAd,
-			id: "crystal-light-liquid-diet-bypass",
-			copy: "Don't cheat: try Crystal Light as a low calorie option",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric bypass journey",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-39topics",
-			copy: "39 gastric bypass topics, all in one e-book",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric bypass post-op questions",
-		},
-	],
-	"how-to-tighten-loose-skin-after-gastric-bypass-without-surgery": [
-		{
-			...proteinInfusedWaterAd,
-			id: "protein-infused-water-loose-skin-without-surgery",
-			copy: "Fill your daily protein quota with protein infused water",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric bypass journey",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-39topics",
-			copy: "39 gastric bypass topics, all in one e-book",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric bypass post-op questions",
-		},
-	],
-	"liquid-diet-before-gastric-bypass-surgery": [
-		{
-			...proteinInfusedWaterAd,
-			id: "protein-infused-water-liquid-bypass",
-			copy: "Fill your daily protein quota with protein infused water",
-		},
-		{
-			...crystalLightAd,
-			id: "crystal-light-liquid-bypass",
-			copy: "Use Crystal Light as a sugar-free, low calorie liquid",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric bypass journey",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-39topics",
-			copy: "39 gastric bypass topics, all in one e-book",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric bypass post-op questions",
-		},
-	],
-	"bariatric-surgery-and-popcorn": [
-		{
-			...proteinShakesAd,
-			id: "protein-shakes-popcorn",
-			copy: "Instead of popcorn, drink protein shakes",
-		},
-	],
-	"gastric-sleeve-regrets": [
-		{
-			...multivitaminAd,
-			id: "sleeve-regrets-multivitamin",
-			copy: "Make sure you're getting the right multivitamins",
-		},
-		{
-			...vsgJourneyEbookAd,
-			id: "vsg-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric sleeve journey",
-		},
-		{
-			...vsgJourneyEbookAd,
-			id: "vsg-journey-ebook-39topics",
-			copy: "39 gastric sleeve topics, all in one e-book",
-		},
-		{
-			...vsgJourneyEbookAd,
-			id: "vsg-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric sleeve post-op questions",
-		},
-	],
-	"gastric-sleeve-and-alcohol": [
-		{
-			...multivitaminAd,
-			id: "sleeve-alcohol-multivitamin",
-			copy: "Make sure you're getting the right multivitamins",
-		},
-		{
-			...vsgJourneyEbookAd,
-			id: "vsg-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric sleeve journey",
-		},
-		{
-			...vsgJourneyEbookAd,
-			id: "vsg-journey-ebook-39topics",
-			copy: "39 gastric sleeve topics, all in one e-book",
-		},
-		{
-			...vsgJourneyEbookAd,
-			id: "vsg-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric sleeve post-op questions",
-		},
-	],
-	"gastric-bypass-pre-op-diet": [
-		{
-			...proteinInfusedWaterAd,
-			id: "protein-infused-water-bypass-preop",
-			copy: "Get some protein infused water as part of your pre-op diet",
-		},
-		{
-			...proteinShakesAd,
-			id: "protein-shakes-bypass-preop",
-			copy: "Get some protein shakes as part of your pre-op diet",
-		},
-		{
-			...crystalLightAd,
-			id: "crystal-light-bypass-preop",
-			copy: "Get some Crystal Light as part of your pre-op diet",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-allinone",
-			copy: "All-in-one e-book for your gastric bypass journey",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-39topics",
-			copy: "39 gastric bypass topics, all in one e-book",
-		},
-		{
-			...rnyJourneyEbookAd,
-			id: "rny-journey-ebook-questions",
-			copy: "An e-book to answer all your gastric bypass post-op questions",
-		},
-	],
+	// "gastric-bypass-pouch-reset": [
+	// 	{
+	// 		...proteinInfusedWaterAd,
+	// 		id: "protein-infused-water-pouch-reset",
+	// 		copy: "Get some protein infused water to assist your reset",
+	// 	},
+	// 	{
+	// 		...proteinShakesAd,
+	// 		id: "protein-shakes-pouch-reset",
+	// 		copy: "Get some protein shakes to assist your reset",
+	// 	},
+	// 	{
+	// 		...crystalLightAd,
+	// 		id: "crystal-light-pouch-reset",
+	// 		copy: "Use Crystal Light as a sugar-free, low calorie liquid",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric bypass journey",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-39topics",
+	// 		copy: "39 gastric bypass topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric bypass post-op questions",
+	// 	},
+	// ],
+	// "liver-shrinking-diet": [
+	// 	{
+	// 		...proteinInfusedWaterAd,
+	// 		id: "protein-infused-water-liver-shrinking",
+	// 		copy: "Get some protein infused water as part of the diet",
+	// 	},
+	// 	{
+	// 		...proteinShakesAd,
+	// 		id: "protein-shakes-liver-shrinking",
+	// 		copy: "Get some protein shakes as part of the diet",
+	// 	},
+	// 	{
+	// 		...crystalLightAd,
+	// 		id: "crystal-light-liver-shrinking",
+	// 		copy: "Use Crystal Light as a sugar-free, low calorie liquid",
+	// 	},
+	// ],
+	// "gastric-bypass-and-hair-loss": [
+	// 	{
+	// 		id: "b12-hair-bypass",
+	// 		link: "https://amzn.to/323j7B0",
+	// 		copy: "Bariatric B12 Vitamins can help prevent hair loss",
+	// 		imgSrc:
+	// 			"https://baribuilder-affiliate-imgs.s3.amazonaws.com/bariatric-advantage-b12.png",
+	// 	},
+	// 	{
+	// 		...proteinShakesAd,
+	// 		id: "protein-shakes-hair-bypass",
+	// 		copy: "Make sure you're getting plenty of protein shakes",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric bypass journey",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-39topics",
+	// 		copy: "39 gastric bypass topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric bypass post-op questions",
+	// 	},
+	// ],
+	// "cheating-on-liquid-diet-before-gastric-bypass": [
+	// 	{
+	// 		...proteinShakesAd,
+	// 		id: "protein-shakes-liquid-diet-bypass",
+	// 		copy: "Don't cheat: drink protein shakes",
+	// 	},
+	// 	{
+	// 		...crystalLightAd,
+	// 		id: "crystal-light-liquid-diet-bypass",
+	// 		copy: "Don't cheat: try Crystal Light as a low calorie option",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric bypass journey",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-39topics",
+	// 		copy: "39 gastric bypass topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric bypass post-op questions",
+	// 	},
+	// ],
+	// "how-to-tighten-loose-skin-after-gastric-bypass-without-surgery": [
+	// 	{
+	// 		...proteinInfusedWaterAd,
+	// 		id: "protein-infused-water-loose-skin-without-surgery",
+	// 		copy: "Fill your daily protein quota with protein infused water",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric bypass journey",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-39topics",
+	// 		copy: "39 gastric bypass topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric bypass post-op questions",
+	// 	},
+	// ],
+	// "liquid-diet-before-gastric-bypass-surgery": [
+	// 	{
+	// 		...proteinInfusedWaterAd,
+	// 		id: "protein-infused-water-liquid-bypass",
+	// 		copy: "Fill your daily protein quota with protein infused water",
+	// 	},
+	// 	{
+	// 		...crystalLightAd,
+	// 		id: "crystal-light-liquid-bypass",
+	// 		copy: "Use Crystal Light as a sugar-free, low calorie liquid",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric bypass journey",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-39topics",
+	// 		copy: "39 gastric bypass topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric bypass post-op questions",
+	// 	},
+	// ],
+	// "bariatric-surgery-and-popcorn": [
+	// 	{
+	// 		...proteinShakesAd,
+	// 		id: "protein-shakes-popcorn",
+	// 		copy: "Instead of popcorn, drink protein shakes",
+	// 	},
+	// ],
+	// "gastric-sleeve-regrets": [
+	// 	{
+	// 		...multivitaminAd,
+	// 		id: "sleeve-regrets-multivitamin",
+	// 		copy: "Make sure you're getting the right multivitamins",
+	// 	},
+	// 	{
+	// 		...vsgJourneyEbookAd,
+	// 		id: "vsg-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric sleeve journey",
+	// 	},
+	// 	{
+	// 		...vsgJourneyEbookAd,
+	// 		id: "vsg-journey-ebook-39topics",
+	// 		copy: "39 gastric sleeve topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...vsgJourneyEbookAd,
+	// 		id: "vsg-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric sleeve post-op questions",
+	// 	},
+	// ],
+	// "gastric-sleeve-and-alcohol": [
+	// 	{
+	// 		...multivitaminAd,
+	// 		id: "sleeve-alcohol-multivitamin",
+	// 		copy: "Make sure you're getting the right multivitamins",
+	// 	},
+	// 	{
+	// 		...vsgJourneyEbookAd,
+	// 		id: "vsg-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric sleeve journey",
+	// 	},
+	// 	{
+	// 		...vsgJourneyEbookAd,
+	// 		id: "vsg-journey-ebook-39topics",
+	// 		copy: "39 gastric sleeve topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...vsgJourneyEbookAd,
+	// 		id: "vsg-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric sleeve post-op questions",
+	// 	},
+	// ],
+	// "gastric-bypass-pre-op-diet": [
+	// 	{
+	// 		...proteinInfusedWaterAd,
+	// 		id: "protein-infused-water-bypass-preop",
+	// 		copy: "Get some protein infused water as part of your pre-op diet",
+	// 	},
+	// 	{
+	// 		...proteinShakesAd,
+	// 		id: "protein-shakes-bypass-preop",
+	// 		copy: "Get some protein shakes as part of your pre-op diet",
+	// 	},
+	// 	{
+	// 		...crystalLightAd,
+	// 		id: "crystal-light-bypass-preop",
+	// 		copy: "Get some Crystal Light as part of your pre-op diet",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your gastric bypass journey",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-39topics",
+	// 		copy: "39 gastric bypass topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...rnyJourneyEbookAd,
+	// 		id: "rny-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your gastric bypass post-op questions",
+	// 	},
+	// ],
+	// default: [
+	// 	{
+	// 		...calciumAd,
+	// 		id: "barimelts-calcium",
+	// 		copy: "Use Bariatric Calcium to keep your bones strong",
+	// 	},
+	// 	{
+	// 		...wlsJourneyEbookAd,
+	// 		id: "wls-journey-ebook-allinone",
+	// 		copy: "All-in-one e-book for your bariatric journey",
+	// 	},
+	// 	{
+	// 		...wlsJourneyEbookAd,
+	// 		id: "wls-journey-ebook-97topics",
+	// 		copy: "97 WLS topics, all in one e-book",
+	// 	},
+	// 	{
+	// 		...wlsJourneyEbookAd,
+	// 		id: "wls-journey-ebook-questions",
+	// 		copy: "An e-book to answer all your post-op questions",
+	// 	},
+	// 	{
+	// 		...mealPlanEbookAd,
+	// 		id: "meal-plan-ebook",
+	// 		copy: "Get a curated 4 week meal plan for after surgery",
+	// 	},
+	// 	{
+	// 		...surgeonAd,
+	// 		id: "find-a-surgeon",
+	// 		copy:
+	// 			"Need help finding a surgeon for your first, or revisional, surgery?",
+	// 	},
+	// ],
 	default: [
 		{
-			...calciumAd,
-			id: "barimelts-calcium",
-			copy: "Use Bariatric Calcium to keep your bones strong",
-		},
-		{
-			...wlsJourneyEbookAd,
-			id: "wls-journey-ebook-allinone",
-			copy: "All-in-one e-book for your bariatric journey",
-		},
-		{
-			...wlsJourneyEbookAd,
-			id: "wls-journey-ebook-97topics",
-			copy: "97 WLS topics, all in one e-book",
-		},
-		{
-			...wlsJourneyEbookAd,
-			id: "wls-journey-ebook-questions",
-			copy: "An e-book to answer all your post-op questions",
-		},
-		{
-			...mealPlanEbookAd,
-			id: "meal-plan-ebook",
-			copy: "Get a curated 4 week meal plan for after surgery",
-		},
-		{
-			...surgeonAd,
-			id: "find-a-surgeon",
+			...backOnTrackEbookAd,
+			id: "back-on-track-ebook-bot",
 			copy:
-				"Need help finding a surgeon for your first, or revisional, surgery?",
+				"Struggling with weight regain? Get back on track with this step-by-step ebook.",
+		},
+		{
+			...bariatricKetoCourse,
+			id: "keto-course-get-started",
+			copy:
+				"Have you heard of the bariatric keto diet? Try our e-learning course to get started.",
+		},
+		{
+			...bariatricKetoEbookAd,
+			id: "keto-book-read-about",
+			copy:
+				"Have you heard of the bariatric keto diet? Read all about it in the BariBuilder ebook.",
+		},
+		{
+			...bariatricKetoEbookAd,
+			id: "keto-book-regain",
+			copy:
+				"Experiencing weight regain after surgery? Consider the bariatric keto diet.",
+		},
+		{
+			...bariatricKetoEbookAd,
+			id: "keto-book-recipes",
+			copy: "Keto for bariatric patients: a book full of recipes.",
+		},
+		{
+			...bariatricKetoSnacks,
+			id: "keto-snacks-subscription",
+			copy:
+				"Enjoy snacking? Make it keto-friendly with our snack subscription box.",
+		},
+		{
+			...bariatricVitaminsEbookAd,
+			id: "vitamins-ebook-overpaying",
+			copy:
+				"You might be overpaying for your vitamins; read more about your options in our ebook.",
+		},
+		{
+			...preOpGuideToBariatricsEbookAd,
+			id: "preop-guide-bariatrics-bases",
+			copy:
+				"Just getting started with weight loss surgery? Read our ebook to cover all your bases.",
 		},
 	],
 };
