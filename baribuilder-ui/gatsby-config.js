92446d622c573f061391a40a2820d8905ff911c6
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2021-07-09 14:00:21 -0700

    Add GTM

--- baribuilder-ui/gatsby-config.js
+++ baribuilder-ui/gatsby-config.js
@@ -207,5 +207,20 @@ module.exports = {
 			},
 		},
 		"gatsby-plugin-styled-components",
+		{
+			resolve: `gatsby-plugin-google-tagmanager`,
+			options: {
+				id: "GTM-KXDZCVC",
+
+				// Include GTM in development.
+				// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+
+				// Specify optional GTM environment details.
+				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+			},
+		},
 	],
 };
