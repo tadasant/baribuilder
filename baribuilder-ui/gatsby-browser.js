d46f49be1e15ca3cba78a5f44136fa55063b881b
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2021-07-09 15:07:33 -0700

    Fix onRenderBody issue

--- baribuilder-ui/gatsby-browser.js
+++ baribuilder-ui/gatsby-browser.js
@@ -30,20 +30,3 @@ var trustAllScripts = function () {
 exports.onRouteUpdate = function () {
 	trustAllScripts();
 };
-
-// Enables cross domain tracking per https://github.com/gatsbyjs/gatsby/issues/13117
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-	const attachCode = `
-if (ga) {
-  ga('require', 'linker');
-  ga('linker:autoLink', ['store.baribuilder.com']);
-}`;
-
-	setPostBodyComponents([
-		<script
-			dangerouslySetInnerHTML={{
-				__html: attachCode,
-			}}
-		/>,
-	]);
-};
---
+++ baribuilder-ui/gatsby-ssr.js
@@ -0,0 +1,16 @@
+// Enables cross domain tracking per https://github.com/gatsbyjs/gatsby/issues/13117
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+	const attachCode = `
+if (ga) {
+  ga('require', 'linker');
+  ga('linker:autoLink', ['store.baribuilder.com']);
+}`;
+
+	setPostBodyComponents([
+		<script
+			dangerouslySetInnerHTML={{
+				__html: attachCode,
+			}}
+		/>,
+	]);
+};
